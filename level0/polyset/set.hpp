5f3727a1a559f1c14f39da565453e69231eab04f
Moiralah <aira.idris@gmail.com>
2026-01-31 12:06:21 +0800

    add polyset

---
+++ level0/polyset/set.hpp
@@ -0,0 +1,24 @@
+
+#ifndef SET_HPP
+#define SET_HPP
+#include "searchable_bag.hpp"
+
+class set
+{
+	private:
+		searchable_bag& bag;
+		
+	public:
+		set() = delete;
+		set(const set& source) = delete;
+		set& operator=(const set& source) = delete;
+		set(searchable_bag& s_bag);
+		bool has(int) const;
+		void insert(int);
+		void insert(int*, int);
+		void print() const;
+		void clear();
+		const searchable_bag& get_bag();
+		~set();
+};
+#endif
